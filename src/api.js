13245fa4982e68d7c3903e7855739696ef63f18d
Gabriel Rocha <rochagbriel@gmail.com>
2023-06-21 22:09:55 +0200

    PWA Config
    - Service worker configuration;
    - Offline mode

--- src/api.js
+++ src/api.js
@@ -76,6 +76,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem('lastEvents');
+    NProgress.done();
+    return events ? JSON.parse(events).events : [];
+  }
+  
   const token = await getAccessToken();
   if (token) {
     removeQuery();
